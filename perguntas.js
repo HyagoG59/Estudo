5f28086735b41a9cc20affaea74e10e8fb0edcff
Hyago <159494212+HyagoG59@users.noreply.github.com>
2024-10-08 11:57:51 -0300

    asd

--- perguntas.js
+++ perguntas.js
@@ -5,20 +5,19 @@ criaCartao(
 )
 
 criaCartao(
-    'Propriedades dos metais',
-    'Como os metais podem ser classificados?',
-    'Os metais podem ser classificados em metais alcalinos, metais alcalino-terrosos, metais de transição, metais pesados e metais nobres, com base em suas propriedades químicas e localização na tabela periódica.'
+    '',
+    '',
+    ''
 )
 
 criaCartao(
-    'Reatividade dos metais',
-    'Como a reatividade dos metais varia na tabela periódica?',
-    'A reatividade dos metais geralmente aumenta à medida que descemos em um grupo da tabela periódica. Por exemplo, os metais alcalinos são altamente reativos, enquanto os metais nobres, como ouro e platina, são muito menos reativos.
-
+    '',
+    '',
+    ''
 )
 
 criaCartao(
-    ' Classificação dos metais',
-    ' Como os metais podem ser classificados?',
+    '',
+    '',
     ''
 )
